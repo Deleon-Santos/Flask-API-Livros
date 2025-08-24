d07a7d4512578c96c244d6b24bdc2fafa0218162
Deleon <delps.santos1987@gmail.com>
2025-08-23 23:09:56 -0300

    feat:criação do bd sqlite 3

--- bd_biblioteca.py
+++ bd_biblioteca.py
@@ -1,6 +1,43 @@
 
-livros = [
-        {'id': 1, 'titulo': 'O Senhor dos Anéis', 'autor': 'J.R.R. Tolkien'},
-        {'id': 2, 'titulo': '1984', 'autor': 'George Orwell'},
-        {'id': 3, 'titulo': 'A Revolução dos Bichos', 'autor': 'George Orwell'}
-    ]
+
+import sqlite3
+def bd():
+    conn = sqlite3.connect('biblioteca.db')
+    return conn
+
+def criar_tabela():
+    conexao = bd()
+    cursor = conexao.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS livros (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        titulo VARCHAR(100) NOT NULL,
+        autor VARCHAR(100) NOT NULL
+    )
+    ''')
+
+    cursor.executemany('''
+    INSERT INTO livros (titulo, autor) VALUES (?, ?)
+    ''', [
+        ('O Senhor dos Anéis', 'J.R.R. Tolkien'),
+        ('1984', 'George Orwell'),
+        ('A Revolução dos Bichos', 'George Orwell')
+    ])
+
+    conexao.commit()
+    conexao.close()
+
+def obter_livros():
+    conexao = bd()
+    cursor = conexao.cursor()
+    cursor.execute('SELECT * FROM livros')
+    livros = cursor.fetchall()
+    conexao.close()
+    lista_livros = []
+    for livro in livros:
+        lista_livros.append({
+            'id': livro[0],
+            'titulo': livro[1],
+            'autor': livro[2]
+        })
+    return lista_livros
--- main.py
+++ main.py
@@ -1,30 +1,43 @@
+# aplicção de cadastro , busca, atualiazação e exclusão de livros em bibliotecas.
+# API isandos Flask e SQL.
+import bd_biblioteca as livros
 from flask import Flask, jsonify, request, make_response
-from bd_biblioteca import livros
 
+
+livros.criar_tabela()
 app = Flask(__name__)
 
+#o metodo get deve retornar todos os livors da nossa base de dados
 @app.route('/livros', methods=['GET'])
-def mostra_livros():    
-    return make_response(jsonify(livros))
+def mostra_livros():  
+    livros_cadastrados = livros.obter_livros()  
+    return make_response(jsonify(livros_cadastrados),200)
 
+#o metodo get vais retornar o livro referenciado no id
+#o respota deve ser um livro encotrado com codigo 200 ou o erro comcodigo 404
 @app.route("/livros/<int:id>",methods= ['GET'])
-def mostra_livros_id(id):
-    
+def mostra_livros_id(id):    
     for livro in livros:
         if livro.get('id') == id:
             return make_response(jsonify(livro),200)
     return make_response(jsonify({'mensagem':'livro não encontrado'}), 404)
 
+#o metodo post deve ler o novo livro no corpo da requisição usando request.get_json()
+#depois adicionar o novo livro na base de dados, ou devolver uma mensagem de erro
 @app.route('/livros', methods=['POST'])
 def cadastrar_livros():
     try:
         novo_livro = request.get_json()
         livros.append(novo_livro)
+        print(livros)
         return make_response(jsonify({"mensagem":"Novo livro cadastrado"},novo_livro),200)
     except Exception as e:
         return make_response(jsonify({"mensagem":"Erro ao cadastrar livro", "erro": str(e)}), 500)
-app.run(debug=True, port =5000)
-
+    
+#metodo para editar um livro pelo id
+#a requisição recebe o um request em json com os dados atualizados.
+#o retorno deve ser o livro atualizado ou uma mensagem de erro
+@app.route('/livros/<int:id>', methods =['PUT'])
 def editar_livros(id):
     try:
         livro_atualizado = request.get_json()
@@ -35,3 +48,21 @@ def editar_livros(id):
         return make_response(jsonify({'mensagem':'livro não encontrado'}), 404)
     except Exception as e:
         return make_response(jsonify({"mensagem":"Erro ao atualizar livro", "erro": str(e)}), 500)
+    
+# o metodo delete deve receber um id da requiseção e se encontrado o livro vais ser deletado
+# o retorno deve ser uma mensagem de sucesso ou uma mensagem de erro
+@app.route('/livros/<int:id>',methods = ['DELETE'])
+def escluir_livro(id):
+    try:        
+        for livro in livros:
+            if livro.get('id') == id:
+                livro_excluido = livro
+                livros.remove(livro)
+                return make_response(jsonify({"mensagem":"Livro excluído com sucesso"}, livro_excluido), 200)
+        return make_response(jsonify({'mensagem':'livro não encontrado'}), 404)
+    except Exception as e:
+        return make_response(jsonify({"mensagem":"Erro ao excluir livro", "erro": str(e)}), 500)
+    
+if __name__ == '__main__':
+    app.run(debug=True,port=5000)
+
